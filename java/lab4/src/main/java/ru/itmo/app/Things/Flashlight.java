33cd35b202573d29b19cff34d2aa5e9e80a5774a
Munir Bairamgulov <91205834+msuny-c@users.noreply.github.com>
2023-12-12 22:20:33 +0300

    Update Flashlight.java

--- java/lab4/src/main/java/ru/itmo/app/Things/Flashlight.java
+++ java/lab4/src/main/java/ru/itmo/app/Things/Flashlight.java
@@ -5,23 +5,22 @@ import ru.itmo.app.Exceptions.LowBatteryException;
 
 public class Flashlight extends Thing {
     private int battery;
+    
     public Flashlight(String name, int battery) throws InvalidBatteryException {
         super(name);
-        if (battery >= 0) {
-            this.battery = battery;
-        } else throw new InvalidBatteryException("Invalid level of battery");
+        if (battery >= 0) {this.battery = battery;} 
+        else throw new InvalidBatteryException("Invalid level of battery");
     }
+    
     public int getBattery() {
-        return battery;
+        return this.battery;
     }
-    public void setModBattery(int amount) {
-        battery += amount;
+    private void setModBattery(int amount) {
+        this.battery += amount;
     }
-    public void light(String something) throws LowBatteryException {
+    public void turnLight(String something) throws LowBatteryException {
         System.out.println(getName() + " высветил " + something);
-        if (getBattery() <= 0) {
-            throw new LowBatteryException("Flashlight has too low battery to use");
-        } else {
-            setModBattery(-1); }
+        if (this.battery > 0) {setModBattery(-1);}
+        else throw new LowBatteryException("Flashlight has too low battery to use");
     }
 }
