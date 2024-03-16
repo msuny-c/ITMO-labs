e44ba81a7f5dc04650b54731dd34a0d2fbf6c588
Munir Bairamgulov <91205834+msuny-c@users.noreply.github.com>
2024-03-16 04:38:27 +0300

    Add files via upload

---
+++ java/lab6/common/src/Models/Mood.java
@@ -0,0 +1,12 @@
+package ru.itmo.app.Models;
+
+import java.io.Serializable;
+
+public enum Mood implements Serializable {
+    SORROW,
+    LONGING,
+    APATHY,
+    CALM,
+    FRENZY;
+    private static final long serialVersionUID = 6521685098267759690L;
+}
