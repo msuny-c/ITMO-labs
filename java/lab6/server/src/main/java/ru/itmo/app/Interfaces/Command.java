3bbb6c26fbd4e5e2410f572c6122e8d8f422153d
Munir Bairamgulov <91205834+msuny-c@users.noreply.github.com>
2024-03-16 04:31:45 +0300

    Add files via upload

--- java/lab6/server/src/main/java/ru/itmo/app/Interfaces/Command.java
+++ java/lab6/server/src/main/java/ru/itmo/app/Interfaces/Command.java
@@ -1,1 +1,10 @@
+package ru.itmo.app.Interfaces;
 
+import ru.itmo.app.Managers.CollectionManager;
+import ru.itmo.app.Models.HumanBeing;
+
+public interface Command {
+    String execute (CollectionManager collectionManager, HumanBeing object, String[] args);
+    int numOfArgs();
+    String getName();
+}
