74715e1515b7a151ef8132e608a21c5a427d85bd
Munir Bairamgulov <91205834+msuny-c@users.noreply.github.com>
2024-03-30 11:52:22 +0300

    Add files via upload

--- java/lab6/client/src/main/java/ru/itmo/app/Managers/ClientManager.java
+++ java/lab6/client/src/main/java/ru/itmo/app/Managers/ClientManager.java
@@ -15,67 +15,70 @@ public class ClientManager {
     private Socket socket;
     private OutputStream outputStream;
     private InputStream inputStream;
+    private boolean isConnected = true;
+    private final byte[] data = new byte[4096];
     public ClientManager(InetAddress address, Integer port) {
         this.address = address;
         this.port = port;
     }
-    public boolean connect(InetAddress address, Integer port) {
+    public void connect(InetAddress address, Integer port) {
         try {
             socket = new Socket(address, port);
             inputStream = socket.getInputStream();
             outputStream = socket.getOutputStream();
-            System.out.println("Connected to server successfully.");
-            return true;
+            System.out.println("The connection to the server was successfully established.");
         } catch (IOException exception) {
-            System.out.println("Error while connecting to the server.");
-            return false;
+            System.out.println("The connection to the server was not established.");
+            isConnected = false;
         }
-    }
-    private boolean reconnect() {
-        for (int i = 1; i < 5 + 1; i++) {
-            System.out.println("Connecting attempt... " + i);
-            if (connect(address, port)) return true;
-        }
-        System.out.println("Failed to connect to the server.");
-        return false;
     }
     public void send(CommandRequest request) {
-        if (socket == null) {
-            System.out.println("Server communication error.");
-            if (reconnect()) {
-                System.out.println("Connected to the server. Resending request...");
-                send(request);
-                return;
-            }
-            System.out.println("Failed to establish connection.");
-            return;
+        if (!isConnected) {
+            try {
+                socket = new Socket(address, port);
+                inputStream = socket.getInputStream();
+                outputStream = socket.getOutputStream();
+            } catch (IOException ignored) {}
         }
         try {
-            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-            ObjectOutputStream oos = new ObjectOutputStream(byteStream);
-            oos.writeObject(request);
-            var header = ByteBuffer.allocate(4).putInt(byteStream.size()).array();
-            outputStream.write(header);
-            outputStream.write(byteStream.toByteArray());
-            byte[] data = new byte[4096];
-            inputStream.read(data);
-            ByteArrayInputStream byteOutputStream = new ByteArrayInputStream(data);
-            ObjectInputStream ois = new ObjectInputStream(byteOutputStream);
-            Response response = (Response) ois.readObject();
-            if (response.error().hasError()) {
-                response.error().printError();
-                System.exit(-1);
-            } else System.out.println(response.message());
-        } catch (SocketException exception) {
-            System.out.println("Server communication error.");
-            if (reconnect()) {
-                System.out.println("Connected to the server. Resending request...");
-                send(request);
+            var serialized = serialize(request);
+            outputStream.write(header(serialized));
+            outputStream.write(serialized);
+            var received = inputStream.read(data);
+            if (received == -1) {
+                System.out.println("The connection to the server is not established.");
+                isConnected = false;
+                return;
             }
-            System.out.println("Failed to establish connection.");
+            Response response = deserialize(data);
+            handle(response);
+            isConnected = true;
+        } catch (SocketException | NullPointerException exception) {
+            System.out.println("The connection to the server is not established.");
+            isConnected = false;
         } catch (IOException | ClassNotFoundException exception) {
-            exception.printStackTrace();
+            System.out.println(exception.getMessage());
+            System.exit(-1);
         }
     }
-
+    private byte[] serialize(CommandRequest request) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(request);
+        return bos.toByteArray();
+    }
+    private Response deserialize(byte[] array) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteOutputStream = new ByteArrayInputStream(array);
+        ObjectInputStream ois = new ObjectInputStream(byteOutputStream);
+        return (Response) ois.readObject();
+    }
+    private byte[] header(byte[] request) {
+        return ByteBuffer.allocate(4).putInt(request.length).array();
+    }
+    private void handle(Response response) {
+        if (response.error().hasError()) {
+            response.error().printError();
+            System.exit(-1);
+        } else System.out.println(response.message());
+    }
 }
