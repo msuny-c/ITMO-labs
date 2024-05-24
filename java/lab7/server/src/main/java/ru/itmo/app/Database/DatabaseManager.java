6ad65b4892f875711d777e083762962ec059378a
Munir Bairamgulov <91205834+msuny-c@users.noreply.github.com>
2024-05-24 15:49:39 +0300

    Add files via upload

--- java/lab7/server/src/main/java/ru/itmo/app/Database/DatabaseManager.java
+++ java/lab7/server/src/main/java/ru/itmo/app/Database/DatabaseManager.java
@@ -22,23 +22,49 @@ public class DatabaseManager<T> {
         auth.put("password", password);
         connection = DriverManager.getConnection(url, auth);
         processor.setConnection(connection);
+        connection.setAutoCommit(false);
     }
-    public int add(T object) throws SQLException {
-        return processor.add(object);
+    public synchronized int add(T object) throws SQLException {
+        try {
+            int id = processor.add(object);
+            connection.commit();
+            return id;
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        }
     }
-    public void remove(Integer id) throws SQLException {
-        processor.remove(id);
+    public synchronized void remove(Integer id) throws SQLException {
+        try {
+            processor.remove(id);
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        }
     }
-    public void update(Integer id, T object) throws SQLException {
-        processor.update(id, object);
+    public synchronized void update(Integer id, T object) throws SQLException {
+        try {
+            processor.update(id, object);
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        }
     }
-    public boolean isPermitted(Integer id, String username) throws SQLException {
+    public synchronized boolean isPermitted(Integer id, String username) throws SQLException {
         return processor.isPermitted(id, username);
     }
-    public void setCollection(List<T> collection) throws SQLException {
-        processor.setCollection(collection);
+    public synchronized void setCollection(List<T> collection) throws SQLException {
+        try {
+            processor.setCollection(collection);
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        }
     }
-    public List<T> getCollection() throws SQLException {
+    public synchronized List<T> getCollection() throws SQLException {
         return processor.getCollection();
     }
     public Connection getConnection() {
